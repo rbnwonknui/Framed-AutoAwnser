c8c2bb05811adae5d3707a43906c033dbcc2f97e
ث rbnwonknui <iunknownbrp@gmail.com>
2025-09-30 01:00:00 -0300

    ✅┃Fix Redirects Erros On The Code

--- Versions/FramedAutoAwnser.js
+++ Versions/FramedAutoAwnser.js
@@ -1,19 +1,19 @@
 const url = window.location.href;
 
-if (url === "https://framed.wtf/archive?day=") {
-    fetch("https://raw.githubusercontent.com/rbnwonknui/Framed-AutoAwnser/refs/heads/main/Versions/Classic.js")
+if (url.includes("https://framed.wtf/archive?day=") && !url.includes("/poster") && !url.includes("/titleshot") && !url.includes("/one-frame")) {
+    fetch("https://raw.githubusercontent.com/danillucas/Framed-AutoAwnser/main/Versions/Classic.js")
         .then(r => r.text())
         .then(r => eval(r));
-} else if (url === "https://framed.wtf/archive/poster?day=") {
-    fetch("https://raw.githubusercontent.com/rbnwonknui/Framed-AutoAwnser/refs/heads/main/Versions/Poster.js")
+} else if (url.includes("https://framed.wtf/archive/poster?day=")) {
+    fetch("https://raw.githubusercontent.com/danillucas/Framed-AutoAwnser/main/Versions/Poster.js")
         .then(r => r.text())
         .then(r => eval(r));
-} else if (url === "https://framed.wtf/archive/titleshot?day=") {
-    fetch("https://raw.githubusercontent.com/rbnwonknui/Framed-AutoAwnser/refs/heads/main/Versions/Titleshot.js")
+} else if (url.includes("https://framed.wtf/archive/titleshot?day=")) {
+    fetch("https://raw.githubusercontent.com/danillucas/Framed-AutoAwnser/main/Versions/Titleshot.js")
         .then(r => r.text())
         .then(r => eval(r));
-} else if (url === "https://framed.wtf/archive/one-frame?day=") {
-    fetch("https://raw.githubusercontent.com/rbnwonknui/Framed-AutoAwnser/refs/heads/main/Versions/Oneframe.js")
+} else if (url.includes("https://framed.wtf/archive/one-frame?day=")) {
+    fetch("https://raw.githubusercontent.com/danillucas/Framed-AutoAwnser/main/Versions/Oneframe.js")
         .then(r => r.text())
         .then(r => eval(r));
 } else {
